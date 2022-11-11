51053a9a8ac7296acb1a60525a3fd6312c38ce2f
IrinaZurina <zurinai@yandex.ru>
2022-11-11 21:24:20 +0300

    Problem_00

---
+++ Seminar_01/Problem_00/Program.cs
@@ -0,0 +1,15 @@
+﻿// Запрашиваем число от пользователя и генерируем рандомное число
+Console.Write("Введите целое число и нажмите enter: ");
+string userInput = Console.ReadLine() ?? "";   //?? "" - проверка на null
+int randomNum = new Random().Next(1, 100);
+
+// преобразование строки в число
+int userNum = int.Parse(userInput);  
+
+// Возводим числа в квадрат
+int sqr_userNum = userNum * userNum;
+int sqr_randomNum = randomNum * randomNum;
+
+// Выводим результаты
+Console.WriteLine($"{userNum} -> {sqr_userNum}");
+Console.WriteLine($"{randomNum} -> {sqr_randomNum}");
