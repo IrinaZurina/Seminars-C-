650019f774b378b6b6b8c16c77d4df3cb3a84e97
IrinaZurina <zurinai@yandex.ru>
2022-11-12 09:56:57 +0300

    First homework, Excercise 6

---
+++ Seminar_01/Homework_01/Ex_06/Program.cs
@@ -0,0 +1,10 @@
+﻿// Запрашиваем число от пользователя
+Console.Write("Введите целое число и нажмите enter: ");
+string userInput = Console.ReadLine() ?? "";  
+
+// преобразование строки в число
+int num = int.Parse(userInput);
+
+// проверяем число на четность
+if (num % 2 == 0) Console.WriteLine($"{num} -> да");
+else Console.WriteLine($"{num} -> нет");
