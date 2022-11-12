2537ce7e4fb5817deb083f3743c52cfe71e25e1d
IrinaZurina <zurinai@yandex.ru>
2022-11-12 09:48:55 +0300

    First homework, Excercise 4

---
+++ Seminar_01/Homework_01/Ex_04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Запрашиваем числа от пользователя
+Console.Write("Введите первое целое число и нажмите enter: ");
+string userInput1 = Console.ReadLine() ?? "";   
+Console.Write("Введите второе целое число и нажмите enter: ");
+string userInput2 = Console.ReadLine() ?? "";  
+Console.Write("Введите тертье целое число и нажмите enter: ");
+string userInput3 = Console.ReadLine() ?? ""; 
+
+// преобразование строки в число
+int num1 = int.Parse(userInput1);  
+int num2 = int.Parse(userInput2); 
+int num3 = int.Parse(userInput3);   
+
+// дополнительная переменная для сохранения максимального числа
+int max = num1;
+
+// сравниваем поочередно числа, новое максимальное записываем в переменную max
+if (num1 > num2) max = num1;
+else max = num2;
+
+if (num3 > max) max = num3;
+
+//выводим максимальное число
+Console.WriteLine($"{num1}, {num2}, {num3} -> {max}");
