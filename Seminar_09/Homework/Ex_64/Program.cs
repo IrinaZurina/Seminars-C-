2ec46d5d4753d93babbdb93298aa865c8d87c852
IrinaZurina <zurinai@yandex.ru>
2022-12-10 10:06:47 +0300

    Seminar 09 Homework

---
+++ Seminar_09/Homework/Ex_64/Program.cs
@@ -0,0 +1,35 @@
+﻿// 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+int start = GetNumberFromUser("Введите целое положительное число: ","Ошибка ввода!");
+
+Console.Write($"N = {start} -> ");
+PrintNaturalNumbers(start, 1);
+
+// метод для вывода натуральных чисел от N до 1
+void PrintNaturalNumbers(int start, int finish)
+{
+    if(start > finish)
+    {
+        Console.Write($"{start}, ");
+        PrintNaturalNumbers(start - 1, finish);
+    }
+    else if (start == finish)
+    {
+        Console.Write($"{start}");
+    }
+}
+
+//  метод получения числа от пользователя
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
